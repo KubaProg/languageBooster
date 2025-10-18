0fdb1cf45ae8f4ebe807998a9f12565fef065b54
Jakub_Opielka <jakub.opielka@q-perior.com>
2025-10-18 14:24:28 +0200

    configured e2e and added first ones to log in check

--- frontend/e2e/app.spec.ts
+++ frontend/e2e/app.spec.ts
@@ -1,6 +1,22 @@
 import { test, expect } from '@playwright/test';
+import { environment } from '../src/environments/environment.staging';
 
 test('should have LanguageBooster as title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle("Frontend");
+});
+
+test('should log in and redirect to the app', async ({ page }) => {
+  // Navigate to the auth page
+  await page.goto('/auth');
+
+  // Fill in the credentials
+  await page.locator('input[formControlName="email"]').fill(environment.e2eUsername);
+  await page.locator('input[formControlName="password"]').fill(environment.e2ePassword);
+
+  // Click the login button
+  await page.locator('button[type="submit"]').click();
+
+  // Wait for navigation and assert the new URL
+  await expect(page).toHaveURL(/.*\/app/);
 });
