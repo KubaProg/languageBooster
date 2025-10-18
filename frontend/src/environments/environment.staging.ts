29cf561e676f553de46032e7bb80ba88d9a93094
Jakub_Opielka <jakub.opielka@q-perior.com>
2025-10-18 19:09:50 +0200

    added debuging to the pull request

---
+++ frontend/src/environments/environment.staging.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  supabaseUrl: '',
+  supabaseKey: '',
+  e2eUsernameId: '6a0bf718-2ba0-43e9-b7d8-c27b172ce499',
+  e2eUsername: '',
+  e2ePassword: ''
+};
