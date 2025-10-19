934002385f8cf0cd2588e200a04e27b5149db9bb
Jakub_Opielka <jakub.opielka@q-perior.com>
2025-10-19 21:45:44 +0200

    added hash locatio nstrayeg

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
