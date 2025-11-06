7278f1b5def675979e075c6f965b89a453d8f6ee
Jakub_Opielka <jakub.opielka@q-perior.com>
2025-11-06 23:46:44 +0100

    edited environment.ts file to let it work fine on localhost

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: process.env["NG_APP_SUPABASE_URL"] ?? '',
-  supabaseKey: process.env["NG_APP_SUPABASE_KEY"] ?? ''
+  supabaseUrl: 'http://127.0.0.1:54321',
+  supabaseKey: 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH'
 };
